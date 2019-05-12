c6998e6f120f6503131975efbcf5c7d8c322ae44
Mario <mizar2502@aol.com>
2019-05-12 11:08:13 +0200

    changed Message Log & Sidebar to instances of Game

--- src/game.ts
+++ src/game.ts
@@ -9,6 +9,9 @@ import { ServiceLocator } from "./service-locator";
 export class Game {
     private display: Display
     private displayOptions: Partial<DisplayOptions>;
+    private messageLog: MessageLog;
+    private sidebar: Sidebar;
+    
     private turns = { max: 25, value: 25 };
 
     constructor() {
@@ -25,36 +28,43 @@ export class Game {
         this.debug();
     }
 
+    getMessageLog(): MessageLog {
+        return this.messageLog;
+    }
+
+    getSidebar(): Sidebar {
+        return this.sidebar;
+    }
+
     private initialize(): void {
         ServiceLocator.provideInputUtility(new InputUtility());
-        ServiceLocator.provideMessageLog(new MessageLog(document.getElementById("messages"), 4));
-        ServiceLocator.provideSidebar(new Sidebar(document.getElementById("sidebar")));
+
+        this.messageLog = new MessageLog(document.getElementById("messages"), 4);
+        this.sidebar = new Sidebar(document.getElementById("sidebar"));
     }
 
     private debug(): Promise<any> {
-        let sidebar = ServiceLocator.getSidebar();
-        sidebar.setLine(LineType.HeaderPlayer, { Left: "@: Player" });
-        sidebar.setLine(LineType.Strength, { Left: "Strength", Right: "11 / 11", BarPercent: 100, BarColor: "crimson" });
-        sidebar.setLine(LineType.Speed, { Left: "Speed", Right: "10 / 10", BarPercent: 100, BarColor: "limegreen" });
-        sidebar.setLine(LineType.Mind, { Left: "Mind", Right: "7 / 7", BarPercent: 100, BarColor: "cornflowerblue" });
-        sidebar.setLine(LineType.HeaderEffects, { Left: "Effects" });
-        sidebar.setLine(LineType.Poison, { Left: "Poison", Right: `${this.turns.value} ${this.turns.value == 1 ? "turn" : "turns"}`, BarPercent: ((this.turns.value / this.turns.max) * 100), BarColor: "slategray" });
+        this.sidebar.setLine(LineType.HeaderPlayer, { Left: "@: Player" });
+        this.sidebar.setLine(LineType.Strength, { Left: "Strength", Right: "11 / 11", BarPercent: 100, BarColor: "crimson" });
+        this.sidebar.setLine(LineType.Speed, { Left: "Speed", Right: "10 / 10", BarPercent: 100, BarColor: "limegreen" });
+        this.sidebar.setLine(LineType.Mind, { Left: "Mind", Right: "7 / 7", BarPercent: 100, BarColor: "cornflowerblue" });
+        this.sidebar.setLine(LineType.HeaderEffects, { Left: "Effects" });
+        this.sidebar.setLine(LineType.Poison, { Left: "Poison", Right: `${this.turns.value} ${this.turns.value == 1 ? "turn" : "turns"}`, BarPercent: ((this.turns.value / this.turns.max) * 100), BarColor: "slategray" });
 
-        ServiceLocator.getMessageLog().addMessages("&nbsp;", "&nbsp;", "&nbsp;", "Press some keys ...");
+        this.messageLog.addMessages("&nbsp;", "&nbsp;", "&nbsp;", "Press some keys ...");
 
         return ServiceLocator.getInputUtility().waitForInput(this.handleInput.bind(this));
     }
 
     private handleInput(event: KeyboardEvent): boolean {
-        ServiceLocator.getMessageLog().addMessages(`Key '${event.code}' was pressed`);
+        this.messageLog.addMessages(`Key '${event.code}' was pressed`);
 
         if (this.turns.value > 0) {
             --this.turns.value;
-            let sidebar = ServiceLocator.getSidebar();
             if (this.turns.value > 0) {
-                sidebar.setLine(LineType.Poison, { Right: `${this.turns.value} ${this.turns.value == 1 ? "turn" : "turns"}`, BarPercent: ((this.turns.value / this.turns.max) * 100) });
+                this.sidebar.setLine(LineType.Poison, { Right: `${this.turns.value} ${this.turns.value == 1 ? "turn" : "turns"}`, BarPercent: ((this.turns.value / this.turns.max) * 100) });
             } else {
-                sidebar.removeLine(LineType.Poison);
+                this.sidebar.removeLine(LineType.Poison);
             }
         }
         return false;
--- src/service-locator.ts
+++ src/service-locator.ts
@@ -1,11 +1,7 @@
 import { InputUtility } from "./util/input-utility";
-import { MessageLog } from "./ui/messsage-log";
-import { Sidebar } from "./ui/sidebar";
 
 export class ServiceLocator {
     private static inputUtility: InputUtility;
-    private static messageLog: MessageLog;
-    private static sidebar: Sidebar;
 
     static getInputUtility(): InputUtility {
         return ServiceLocator.inputUtility;
@@ -13,21 +9,5 @@ export class ServiceLocator {
 
     static provideInputUtility(inputUtility: InputUtility): void {
         ServiceLocator.inputUtility = inputUtility;
-    }
-
-    static getMessageLog(): MessageLog {
-        return ServiceLocator.messageLog;
-    }
-
-    static provideMessageLog(messageLog: MessageLog): void {
-        ServiceLocator.messageLog = messageLog;
-    }
-
-    static getSidebar(): Sidebar {
-        return ServiceLocator.sidebar;
-    }
-
-    static provideSidebar(sidebar: Sidebar): void {
-        ServiceLocator.sidebar = sidebar;
     }
 }
